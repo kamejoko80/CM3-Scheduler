5fa10e7488f90c6a01451c40549d80edd78fd8cd
Will <w.pond@sms.ed.ac.uk>
2012-11-10 02:11:32 +0000

    initial commit (working scheduler)

---
+++ main.c
@@ -0,0 +1,107 @@
+
+#include "efm32.h"
+
+#include "efm32_chip.h"
+#include "efm32_rtc.h"
+#include "efm32_gpio.h"
+#include "efm32_cmu.h"
+#include "efm32_timer.h"
+#include "efm32_int.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "scheduler.h"
+#include "led.h"
+
+void initClocks();
+void enableTimers();
+void enableInterrupts();
+
+int main()
+{
+	
+	// Chip errata
+	CHIP_Init();
+	
+	// ensure core frequency has been updated
+	SystemCoreClockUpdate();
+	
+	// start clocks
+	InitClocks();
+		
+	// init LEDs
+	LED_Init();
+	
+	// init scheduler
+	SCHEDULER_Init();
+	
+	// enable timers
+	enableTimers();
+	
+	// enable interrupts
+	enableInterrupts();
+	
+	// init tasks
+	// SCHEDULER_TaskInit(task, entrypoint);
+	
+	// run
+	SCHEDULER_Run();
+	
+}
+
+void enableTimers()
+{
+	
+}
+
+void enableInterrupts()
+{
+	
+	NVIC_EnableIRQ(SysTick_IRQn);
+	NVIC_EnableIRQ(PendSV_IRQn);
+	
+}
+
+void initClocks()
+{
+	
+	/* Starting LFXO and waiting until it is stable */
+	CMU_OscillatorEnable(cmuOsc_LFXO, true, true);
+
+	// starting HFXO, wait till stable
+	CMU_OscillatorEnable(cmuOsc_HFXO, true, true);
+
+	// route HFXO to CPU
+	CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
+
+	/* Routing the LFXO clock to the RTC */
+	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
+	CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_LFXO);
+
+	// disabling the RCs
+	CMU_ClockEnable(cmuSelect_HFRCO, false);
+	CMU_ClockEnable(cmuSelect_LFRCO, false);
+
+	/* Enabling clock to the interface of the low energy modules */
+	CMU_ClockEnable(cmuClock_CORE, true);
+	CMU_ClockEnable(cmuClock_CORELE, true);
+
+	// enable clock to hf perfs
+	CMU_ClockEnable(cmuClock_HFPER, true);
+
+	// enable clock to GPIO
+	CMU_ClockEnable(cmuClock_GPIO, true);
+
+	// enable clock to RTC
+	CMU_ClockEnable(cmuClock_RTC, true);
+	RTC_Enable(true);
+
+	// enable radio usart
+	CMU_ClockEnable(cmuClock_USART0, true);
+
+	// enable timers
+	CMU_ClockEnable(cmuClock_TIMER0, true);
+	CMU_ClockEnable(cmuClock_TIMER1, true);
+
+}
